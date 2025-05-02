f91046764a8f8dc67cd11802338010ffb0ceb418
Ashley Lin <linashleyy@gmail.com>
2025-05-03 03:16:44 +0800

    Get upcoming parties data from api

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
+// === Initial states ===
+
+let upcomingParties = [];
+let selectedParty;
+
+// === API Info ===
+
+const url = "https://fsa-crud-2aa9294fe819.herokuapp.com/api";
+const cohort = "/2109-CPU-RM-WEB-PT";
+const resource = "/events";
+const api = url + cohort + resource;
+
+// === Fetch upcoming parties data from api ===
+
+async function getParties() {
+  try {
+    const response = await fetch(api);
+    const data = await response.json();
+    upcomingParties = data.data;
+    console.log(upcomingParties); // delete this later
+  } catch (error) {
+    console.log(`ERROR: ${error}`);
+  }
+}
+getParties();
+render();
+
 // === Render ===
 
 function render() {
